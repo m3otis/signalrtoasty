68bae9314445fdc1e2c18c04bce645fc8cd693f5
Mitchell Sitohang <mitchellsitohang@me.com>
2019-09-30 15:49:02 +0200

    changes

---
+++ SignalR/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using SignalR.Hubs;
+
+namespace SignalR.Controllers
+{
+    public class TestController : Controller
+    {
+        private readonly IHubContext<ChatHub> hc;
+        
+        public TestController(IHubContext<ChatHub> hc) {
+            this.hc = hc;
+        }
+
+        public IActionResult Index()
+        {
+            hc.Clients.All.SendAsync("messageReceived", "TEST1", "TEST2");
+            return Ok("toasty");
+        }
+    }
+}
